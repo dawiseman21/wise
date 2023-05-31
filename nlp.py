54a39f272819ae7f3c448dfe80cf5385d48d2fd3
dawiseman21 <134638533+dawiseman21@users.noreply.github.com>
2023-05-31 15:37:08 -0600

    misc ideas

---
+++ nlp.py
@@ -0,0 +1,8 @@
+
+from semantic import analyze
+
+text = "Semantic information extraction is the process of extracting specific data from text and enriching it with machine-readable links, references, and relationships."
+
+result = analyze(text)
+
+print(result)
